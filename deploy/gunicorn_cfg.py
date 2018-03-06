9694a431dd4cf7a0dd81e202382a02f881280ab5
Luke Renegar <luke.renegar@gmail.com>
2018-03-06 06:01:49 +0000

    Added logging to gunicorn configuration

--- deploy/gunicorn_cfg.py
+++ deploy/gunicorn_cfg.py
@@ -2,4 +2,7 @@ import multiprocessing
 
 bind = "unix:/run/tawhiri/v1.1.sock"
 pidfile = "/run/tawhiri/v1.1.pid"
+accesslog = "/var/log/tawhiri/develop-access.log"
+errorlog = "/var/log/tawhiri/develop-error.log"
 workers = 3
+capture_output = "True"
